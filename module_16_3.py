49f79e5428a4238ccb79c456c18e752e3cc0119b
wh1tepr1nc3 <amamamania@mail.ru>
2024-12-29 00:21:05 +0500

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -19,34 +19,31 @@ async def get_users() -> dict:
 
 
 @app.post('/user/{username}/{age}')
-async def create_user(username: Annotated[str, Path(min_length=5,
-                                                   max_length=20,
-                                                   description='Enter username',
-                                                   examples='UrbanUser')],
-                     age: Annotated[int, Path(ge=18,
-                                              le=120,
-                                              description='Enter age',
-                                              examples=24)]):
-    user_id = str(int(max(users, key=int)) + 1)
-    users[user_id] = f'Имя: {username}, возраст: {age}'
-    return f'User {user_id} is registered'
+async def post_user(
+        username: Annotated[str,
+            Path(max_length=30, description='Введите имя', examples='Vasiliy')],
+        age: Annotated[int,
+            Path(le=120, description='Введите возраст', examples='20')]
+) -> str:
+    new_user_id = str(int(max(users, key=int)) + 1)
+    users[new_user_id] = f'Имя: {username}: Возраст: {age}'
+    return f'Пользователь {new_user_id} зарегестрирован'
 
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: str,
-                      username: Annotated[str, Path(min_length=5,
-                                                   max_length=20,
-                                                   description='Enter username',
-                                                   examples='UrbanUser')],
-                      age: Annotated[int, Path(ge=18,
-                                              le=120,
-                                              description='Enter age',
-                                              examples=24)]):
+async def put_users(
+        user_id: int,
+        username: Annotated[str,
+        Path(max_length=30, description='Введите свое имя', examples='Vasiliy')],
+        age: Annotated[int,
+        Path(ge=18, le=120, description='Введите возраст', examples='24')]
+) -> str:
     users[user_id] = f'Имя: {username}, возраст: {age}'
-    return f'The user {user_id} is updated'
+    return f'Пользователь {user_id} обновлен'
 
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: str):
+async def del_users(user_id: Annotated[str,
+Path(description='Введите ID для удаления', examples='1')]) -> str:
     users.pop(user_id)
-    return f'User {user_id} has been deleted'
+    return f'Пользователь {user_id} удален'
